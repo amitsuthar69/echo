42ba0157f83c2598ccdcd6d00c20fc988d677ae7
sumitst05 <sumitst05@gmail.com>
2024-06-13 23:13:04 +0530

    feat: add events and store them in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import AddEvent from "./components/AddEvent"
 import ToggleTheme from "./components/ToggleTheme"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
 		<div className="flex flex-col items-center min-h-screen bg-[#F8F6E3] dark:bg-[#1B262C] gap-4 select-none">
 			<h1 className="text-7xl font-mono text-[#7AA2E3] dark:text-[#3282B8] mt-6">Echo</h1>
 			<ToggleTheme />
+			<AddEvent />
 		</div>
 	)
 }
