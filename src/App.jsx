447752dcc2f899b56000093d40ab8bfd9186e9d4
sumitst05 <sumitst05@gmail.com>
2024-06-13 21:20:10 +0530

    feat: implemented theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,11 @@
+import ToggleTheme from "./components/ToggleTheme"
+
 function App() {
 	return (
-		<div>App</div>
+		<div className="flex flex-col items-center min-h-screen bg-[#F8F6E3] dark:bg-[#1B262C] gap-4 select-none">
+			<h1 className="text-7xl font-mono text-[#7AA2E3] dark:text-[#3282B8] mt-6">Echo</h1>
+			<ToggleTheme />
+		</div>
 	)
 }
 
