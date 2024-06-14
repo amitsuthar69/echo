cbac24f7218f19bb9967d62ab430b147431d7bac
Amit Suthar <sutharamit707@gmail.com>
2024-06-14 13:55:23 +0530

    Feat: delete event

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,10 +1,24 @@
-import { useState } from "react";
+import { React, useState } from "react";
 
 export default function SideBar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [events, setEvents] = useState([]);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
+    const e = JSON.parse(localStorage.getItem("events"));
+    setEvents(e);
+  };
+
+  const handleDelete = (id) => {
+    const updatedEvents = events.filter((_, i) => i !== id);
+    setEvents(updatedEvents);
+    localStorage.setItem("events", JSON.stringify(updatedEvents));
+  };
+
+  const handleEdit = (id) => {
+    const currentEvent = events.filter((_, i) => i == id);
+    console.log("curr: ", currentEvent);
   };
 
   return (
@@ -22,9 +36,17 @@ export default function SideBar() {
         }`}
       >
         <ul className="mt-20">
-          <li className="p-4">Item 1</li>
-          <li className="p-4">Item 2</li>
-          <li className="p-4">Item 3</li>
+          {events &&
+            events.map((e, i) => (
+              <div key={i}>
+                <EventCard
+                  id={i}
+                  title={e.title}
+                  handleDelete={handleDelete}
+                  handleEdit={handleEdit}
+                />
+              </div>
+            ))}
         </ul>
       </aside>
     </div>
@@ -72,6 +94,78 @@ function HamburgerMenu({ toggleSidebar }) {
           </>
         )}
       </svg>
+    </div>
+  );
+}
+
+function EventCard({ id, title, handleDelete, handleEdit }) {
+  return (
+    <div className="flex mt-2 items-center justify-between py-4 px-3 mx-4 rounded-md dark:text-gray-50 text-dark-blue-400 bg-white dark:bg-light-blue-400 hover:scale-105 transition-transform duration-300 cursor-pointer">
+      <p className="font-bold overflow-hidden text-ellipsis whitespace-nowrap w-full">
+        {title}
+      </p>
+      <div className="btns flex gap-1 items-center justify-end">
+        <button className="hover:bg-gray-200 p-1.5 rounded">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#333333"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-pin"
+          >
+            <line x1="12" x2="12" y1="17" y2="22" />
+            <path d="M5 17h14v-1.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V6h1a2 2 0 0 0 0-4H8a2 2 0 0 0 0 4h1v4.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24Z" />
+          </svg>
+        </button>
+        <button
+          onClick={() => handleEdit(id)}
+          className="hover:bg-purple-200 dark:hover:bg-purple-200 p-1.5 rounded"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#59ff00"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-pencil"
+          >
+            <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+            <path d="m15 5 4 4" />
+          </svg>
+        </button>
+        <button
+          onClick={() => handleDelete(id)}
+          className="hover:bg-red-100 dark:hover:bg-red-100 p-1.5 rounded"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="20"
+            height="20"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#ff4d4d"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            className="lucide lucide-trash-2"
+          >
+            <path d="M3 6h18" />
+            <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+            <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+            <line x1="10" x2="10" y1="11" y2="17" />
+            <line x1="14" x2="14" y1="11" y2="17" />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 }
