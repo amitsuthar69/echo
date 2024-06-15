22016a7eb21e8f2ea2a1faa616405fdf144a2857
sumitst05 <sumitst05@gmail.com>
2024-06-15 10:01:03 +0530

    feat: edit pinned events

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -1,27 +1,52 @@
-import { useState } from "react";
+import React, { useState, useEffect } from 'react';
 
-function EventForm({ onClose, onSave }) {
-	const [eventDetails, setEventDetails] = useState({});
+function EventForm({ onClose, onSave, eventData }) {
+	const [eventDetails, setEventDetails] = useState({
+		title: '',
+		description: '',
+		date: ''
+	});
+
+	useEffect(() => {
+		if (eventData) {
+			// Initialize form fields with eventData if provided
+			setEventDetails(eventData);
+		} else {
+			// Reset form fields when eventData is null (for new event)
+			setEventDetails({
+				title: '',
+				description: '',
+				date: ''
+			});
+		}
+	}, [eventData]);
 
 	const handleChange = (e) => {
-		setEventDetails(eventDetails => ({ ...eventDetails, [e.target.name]: e.target.value }));
-	}
+		const { name, value } = e.target;
+		setEventDetails((prevDetails) => ({ ...prevDetails, [name]: value }));
+	};
 
-	const handleSave = () => {
+	const handleSubmit = (e) => {
+		e.preventDefault();
+
+		// Validate form fields
 		if (eventDetails.title && eventDetails.description && eventDetails.date) {
-			onSave(eventDetails);
-			onClose();
+			onSave(eventDetails); // Save eventDetails
+			onClose(); // Close the form
 		}
-	}
+	};
 
 	return (
 		<div className="fixed top-0 left-0 w-full h-full px-2 flex justify-center items-center bg-gray-500 bg-opacity-50 z-50">
-			<form className="bg-[#7AA2E3] dark:bg-[#3282B8] p-8 rounded-lg">
-				<h2 className="text-xl text-[#F8F6E3] dark:text-[#1B262C] font-bold mb-4">Add New Event</h2>
+			<form onSubmit={handleSubmit} className="bg-[#7AA2E3] dark:bg-[#3282B8] p-8 rounded-lg">
+				<h2 className="text-xl text-[#F8F6E3] dark:text-[#1B262C] font-bold mb-4">
+					{eventData ? 'Edit Event' : 'Add New Event'}
+				</h2>
 				<input
 					name="title"
 					type="text"
 					required
+					value={eventDetails.title}
 					onChange={handleChange}
 					placeholder="Title"
 					className="rounded-lg border border-gray-300 p-2 mb-4 w-full dark:bg-[#BBE1FA]"
@@ -30,6 +55,7 @@ function EventForm({ onClose, onSave }) {
 					name="description"
 					onChange={handleChange}
 					required
+					value={eventDetails.description}
 					placeholder="Description"
 					className="rounded-lg border border-gray-300 p-2 mb-4 w-full dark:bg-[#BBE1FA]"
 				/>
@@ -37,18 +63,19 @@ function EventForm({ onClose, onSave }) {
 					name="date"
 					type="datetime-local"
 					required
+					value={eventDetails.date}
 					onChange={handleChange}
 					className="rounded-lg border border-gray-300 p-2 mb-4 w-full dark:bg-[#BBE1FA]"
 				/>
 				<div className="flex justify-end">
 					<button
-						onClick={handleSave}
+						type="submit"
 						className="bg-[#BBE1FA] text-slate-800 px-4 py-2 rounded-md mr-2"
 					>
 						Save
 					</button>
 					<button
-						type="submit"
+						type="button"
 						onClick={onClose}
 						className="bg-red-300 text-gray-700 px-4 py-2 rounded-md"
 					>
@@ -57,7 +84,7 @@ function EventForm({ onClose, onSave }) {
 				</div>
 			</form>
 		</div>
-	)
+	);
 }
 
 export default EventForm;
