11d1240f44ff649126978982a58c066c003f9e1f
Amit Suthar <sutharamit707@gmail.com>
2024-06-15 10:13:51 +0530

    feat: pin events and party poppers

--- src/components/EventModal.jsx
+++ src/components/EventModal.jsx
@@ -1,84 +1,115 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
+import ConfettiExplosion from "react-confetti-explosion";
 
 function EventModal({ event, onClose, gradient }) {
-	const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining(event.date));
+  const [timeRemaining, setTimeRemaining] = useState(
+    calculateTimeRemaining(event.date)
+  );
 
-	const gradients = [
-		"linear-gradient(to right, #bb377d, #fbd3e9)",
-		"linear-gradient(to right, #a1c4fd, #c2e9fb)",
-		"linear-gradient(to right, #f6d365, #fda085)",
-		"linear-gradient(to right, #ff9a9e, #fad0c4)",
-		"linear-gradient(to right, #a8edea, #fed6e3)",
-	];
+  const gradients = [
+    "linear-gradient(to right, #bb377d, #fbd3e9)",
+    "linear-gradient(to right, #a1c4fd, #c2e9fb)",
+    "linear-gradient(to right, #f6d365, #fda085)",
+    "linear-gradient(to right, #ff9a9e, #fad0c4)",
+    "linear-gradient(to right, #a8edea, #fed6e3)",
+  ];
 
-	// Function to calculate time remaining until event starts
-	function calculateTimeRemaining(eventDate) {
-		const now = new Date();
-		const eventDateTime = new Date(eventDate);
-		let difference = eventDateTime - now;
+  // Function to calculate time remaining until event starts
+  function calculateTimeRemaining(eventDate) {
+    const now = new Date();
+    const eventDateTime = new Date(eventDate);
+    let difference = eventDateTime - now;
 
-		// If the event date is in the past, set time remaining to 0
-		if (difference < 0) {
-			difference = 0;
-		}
+    // If the event date is in the past, set time remaining to 0
+    if (difference < 0) {
+      difference = 0;
+    }
 
-		return difference;
-	}
+    return difference;
+  }
 
-	// Format time remaining for display
-	const formatTimeRemaining = () => {
-		let remaining = timeRemaining / 1000; // Convert to seconds
+  // Format time remaining for display
+  const formatTimeRemaining = () => {
+    let remaining = timeRemaining / 1000; // Convert to seconds
 
-		const seconds = Math.floor(remaining % 60);
-		remaining /= 60; // Convert to minutes
-		const minutes = Math.floor(remaining % 60);
-		remaining /= 60; // Convert to hours
-		const hours = Math.floor(remaining % 24);
-		remaining /= 24; // Convert to days
-		const days = Math.floor(remaining);
+    const seconds = Math.floor(remaining % 60);
+    remaining /= 60; // Convert to minutes
+    const minutes = Math.floor(remaining % 60);
+    remaining /= 60; // Convert to hours
+    const hours = Math.floor(remaining % 24);
+    remaining /= 24; // Convert to days
+    const days = Math.floor(remaining);
 
-		if (days > 0) {
-			return `${days}d ${hours}h ${minutes}m ${seconds}s`;
-		} else if (hours > 0) {
-			return `${hours}h ${minutes}m ${seconds}s`;
-		} else if (minutes > 0) {
-			return `${minutes}m ${seconds}s`;
-		} else {
-			return `${seconds}s`;
-		}
-	};
+    if (days > 0) {
+      return `${days}d ${hours}h ${minutes}m ${seconds}s`;
+    } else if (hours > 0) {
+      return `${hours}h ${minutes}m ${seconds}s`;
+    } else if (minutes > 0) {
+      return `${minutes}m ${seconds}s`;
+    } else {
+      return `${seconds}s`;
+    }
+  };
 
-	// Function to update time remaining every second
-	useEffect(() => {
-		const timer = setInterval(() => {
-			setTimeRemaining(calculateTimeRemaining(event.date));
-		}, 1000);
-		return () => clearInterval(timer);
-	}, [event.date]);
+  // Function to update time remaining every second
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTimeRemaining(calculateTimeRemaining(event.date));
+    }, 1000);
+    return () => clearInterval(timer);
+  }, [event.date]);
 
-	// Randomly select a gradient if not provided
-	const getRandomGradient = () => {
-		const randomIndex = Math.floor(Math.random() * gradients.length);
-		return gradients[randomIndex];
-	};
+  // Randomly select a gradient if not provided
+  const getRandomGradient = () => {
+    const randomIndex = Math.floor(Math.random() * gradients.length);
+    return gradients[randomIndex];
+  };
 
-	// Use the provided gradient or a random one
-	const modalGradient = gradient || getRandomGradient();
+  // Use the provided gradient or a random one
+  const modalGradient = gradient || getRandomGradient();
 
-	return (
-		<div className="fixed top-0 left-0 w-full h-full px-2 flex justify-center items-center bg-gray-500 bg-opacity-50 z-50">
-			<div className="event-details bg-white rounded-lg shadow-lg p-6 max-w-md overflow-hidden" style={{ background: modalGradient }}>
-				<div className="countdown-container text-6xl font-bold text-gray-800 mb-6">
-					{formatTimeRemaining()}
-				</div>
-				<h2 className="text-3xl font-semibold text-gray-800 mb-2">{event.title}</h2>
-				<div className="description-container text-gray-800 text-2xl font-mono mb-4 overflow-y-auto" style={{ height: '100px', width: '350px' }}>
-					{event.description}
-				</div>
-				<button onClick={onClose} className="mt-4 px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700 transition duration-300">Close</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="fixed top-0 left-0 w-full h-full px-2 flex justify-center items-center bg-gray-500 bg-opacity-50 z-40">
+      <div
+        className="event-details bg-white rounded-lg shadow-lg p-6 max-w-md overflow-hidden"
+        style={{ background: modalGradient }}
+      >
+        {timeRemaining == 0 && (
+          <h1 className="font-bold text-lg text-gray-800 mb-2">
+            This Event has ended!!!
+          </h1>
+        )}
+        <div className="countdown-container text-6xl font-bold text-gray-800 mb-6">
+          {formatTimeRemaining()}
+        </div>
+        <h2 className="text-3xl font-semibold text-gray-800 mb-2">
+          {event.title}
+        </h2>
+        <div className="z-50">
+          {timeRemaining == 0 && (
+            <ConfettiExplosion
+              force={0.8}
+              particleCount={250}
+              width={1600}
+              duration={3000}
+            />
+          )}
+        </div>
+        <div
+          className="description-container text-gray-800 text-2xl font-mono overflow-y-auto"
+          style={{ height: "100px", width: "350px" }}
+        >
+          {event.description}
+        </div>
+        <button
+          onClick={onClose}
+          className="mt-4 px-4 py-2 bg-gray-800 text-white rounded hover:bg-gray-700 transition duration-300"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default EventModal;
--- src/components/PinnedEventsCarousal.jsx
+++ src/components/PinnedEventsCarousal.jsx
@@ -1,244 +1,247 @@
-import { useState, useEffect } from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import 'react-alice-carousel/lib/alice-carousel.css';
-import moment from 'moment';
-import EventModal from './EventModal';
-import EventForm from './EventForm';
+import { useState, useEffect } from "react";
+import AliceCarousel from "react-alice-carousel";
+import "react-alice-carousel/lib/alice-carousel.css";
+import moment from "moment";
+import EventModal from "./EventModal";
+import EventForm from "./EventForm";
 
 function PinnedEventsCarousel() {
-	const events = JSON.parse(localStorage.getItem('events')) || [];
-	const [pinnedEventIndexes, setPinnedEventIndexes] = useState(
-		JSON.parse(localStorage.getItem('pinnedEventIndexes')) || []
-	);
-	const formatDate = (date) => moment(date).format('MMMM Do YYYY, h:mm:ss a');
+  const events = JSON.parse(localStorage.getItem("events")) || [];
+  const [pinnedEventIndexes, setPinnedEventIndexes] = useState(
+    JSON.parse(localStorage.getItem("pinnedEventIndexes")) || []
+  );
+  const formatDate = (date) => moment(date).format("MMMM Do YYYY, h:mm:ss a");
 
-	const [itemsToShow, setItemsToShow] = useState(1);
-	const [selectedEvent, setSelectedEvent] = useState(null);
-	const [selectedEventIndex, setSelectedEventIndex] = useState(0);
-	const [modalOpen, setModalOpen] = useState(false);
-	const [selectedGradient, setSelectedGradient] = useState(null);
+  const [itemsToShow, setItemsToShow] = useState(1);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [selectedEventIndex, setSelectedEventIndex] = useState(0);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedGradient, setSelectedGradient] = useState(null);
 
-	const [formOpen, setFormOpen] = useState(false);
+  const [formOpen, setFormOpen] = useState(false);
 
-	useEffect(() => {
-		const updateItemsToShow = () => {
-			if (window.innerWidth >= 1024) {
-				setItemsToShow(3);
-			} else if (window.innerWidth >= 600) {
-				setItemsToShow(2);
-			} else {
-				setItemsToShow(1);
-			}
-		};
+  useEffect(() => {
+    const updateItemsToShow = () => {
+      if (window.innerWidth >= 1024) {
+        setItemsToShow(3);
+      } else if (window.innerWidth >= 600) {
+        setItemsToShow(2);
+      } else {
+        setItemsToShow(1);
+      }
+    };
 
-		window.addEventListener('resize', updateItemsToShow);
-		updateItemsToShow();
+    window.addEventListener("resize", updateItemsToShow);
+    updateItemsToShow();
 
-		const handleStorageChange = (event) => {
-			if (event.key === 'pinnedEventIndexes') {
-				setPinnedEventIndexes(JSON.parse(event.newValue));
-			}
-		};
+    const handleStorageChange = (event) => {
+      if (event.key === "pinnedEventIndexes") {
+        setPinnedEventIndexes(JSON.parse(event.newValue));
+      }
+    };
 
-		window.addEventListener('storage', handleStorageChange);
+    window.addEventListener("storage", handleStorageChange);
 
-		return () => {
-			window.removeEventListener('resize', updateItemsToShow);
-			window.removeEventListener('storage', handleStorageChange);
-		};
-	}, []);
+    return () => {
+      window.removeEventListener("resize", updateItemsToShow);
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
 
-	const openModal = (event, gradient) => {
-		setSelectedEvent(event);
-		setSelectedGradient(gradient);
-		setModalOpen(true);
-	};
+  const openModal = (event, gradient) => {
+    setSelectedEvent(event);
+    setSelectedGradient(gradient);
+    setModalOpen(true);
+  };
 
-	const closeModal = () => {
-		setModalOpen(false);
-	};
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
-	const openEditForm = (eventIndex) => {
-		setSelectedEvent(events[eventIndex]);
-		setSelectedEventIndex(eventIndex);
-		setFormOpen(true);
-	};
+  const openEditForm = (eventIndex) => {
+    setSelectedEvent(events[eventIndex]);
+    setSelectedEventIndex(eventIndex);
+    setFormOpen(true);
+  };
 
-	const closeForm = () => {
-		setFormOpen(false);
-		setSelectedEvent(null);
-	};
+  const closeForm = () => {
+    setFormOpen(false);
+    setSelectedEvent(null);
+  };
 
-	const gradients = [
-		"linear-gradient(to right, #bb377d, #fbd3e9)",
-		"linear-gradient(to right, #a1c4fd, #c2e9fb)",
-		"linear-gradient(to right, #f6d365, #fda085)",
-		"linear-gradient(to right, #ff9a9e, #fad0c4)",
-		"linear-gradient(to right, #a8edea, #fed6e3)",
-	];
+  const gradients = [
+    "linear-gradient(to right, #bb377d, #fbd3e9)",
+    "linear-gradient(to right, #a1c4fd, #c2e9fb)",
+    "linear-gradient(to right, #f6d365, #fda085)",
+    "linear-gradient(to right, #ff9a9e, #fad0c4)",
+    "linear-gradient(to right, #a8edea, #fed6e3)",
+  ];
 
-	const getGradient = (index) => gradients[index % gradients.length];
+  const getGradient = (index) => gradients[index % gradients.length];
 
-	const groups = [];
-	for (let i = 0; i < pinnedEventIndexes.length; i += itemsToShow) {
-		groups.push(pinnedEventIndexes.slice(i, i + itemsToShow));
-	}
+  const groups = [];
+  for (let i = 0; i < pinnedEventIndexes.length; i += itemsToShow) {
+    groups.push(pinnedEventIndexes.slice(i, i + itemsToShow));
+  }
 
-	const deletePinnedEvent = (eventIndex) => {
-		const updatedEvents = events.filter((_, index) => index !== eventIndex);
-		localStorage.setItem('events', JSON.stringify(updatedEvents));
+  const deletePinnedEvent = (eventIndex) => {
+    const updatedEvents = events.filter((_, index) => index !== eventIndex);
+    localStorage.setItem("events", JSON.stringify(updatedEvents));
 
-		const updatedPinnedIndexes = pinnedEventIndexes.filter((index) => index !== eventIndex);
+    const updatedPinnedIndexes = pinnedEventIndexes.filter(
+      (index) => index !== eventIndex
+    );
 
-		const updatedIndexes = updatedPinnedIndexes.map(index => {
-			if (index > eventIndex) {
-				return index - 1;
-			}
-			return index;
-		});
+    const updatedIndexes = updatedPinnedIndexes.map((index) => {
+      if (index > eventIndex) {
+        return index - 1;
+      }
+      return index;
+    });
 
-		localStorage.setItem('pinnedEventIndexes', JSON.stringify(updatedIndexes));
-		setPinnedEventIndexes(updatedIndexes);
+    localStorage.setItem("pinnedEventIndexes", JSON.stringify(updatedIndexes));
+    setPinnedEventIndexes(updatedIndexes);
 
-		if (selectedEvent && selectedEvent.index === eventIndex) {
-			closeModal();
-		}
-	};
+    if (selectedEvent && selectedEvent.index === eventIndex) {
+      closeModal();
+    }
+  };
 
-	const handleSave = (updatedEvent) => {
-		const updatedEvents = [...events]; // Copy the events array
-		const eventIndex = selectedEventIndex; // Get the selected event index
+  const handleSave = (updatedEvent) => {
+    const updatedEvents = [...events]; // Copy the events array
+    const eventIndex = selectedEventIndex; // Get the selected event index
 
-		if (eventIndex >= 0 && eventIndex < updatedEvents.length) {
-			updatedEvents[eventIndex] = updatedEvent; // Update the event at the correct index
-			localStorage.setItem('events', JSON.stringify(updatedEvents)); // Update localStorage
+    if (eventIndex >= 0 && eventIndex < updatedEvents.length) {
+      updatedEvents[eventIndex] = updatedEvent; // Update the event at the correct index
+      localStorage.setItem("events", JSON.stringify(updatedEvents)); // Update localStorage
 
-			// Update selectedEvent if it matches the edited event
-			setSelectedEvent(updatedEvent); // Update selectedEvent with updatedEvent
-		}
+      // Update selectedEvent if it matches the edited event
+      setSelectedEvent(updatedEvent); // Update selectedEvent with updatedEvent
+    }
 
-		closeForm();
-	};
+    closeForm();
+  };
 
-	return (
-		<>
-			<AliceCarousel
-				autoPlay
-				autoPlayInterval={3000}
-				infinite
-				items={groups.map((group, index) => (
-					<div key={index} className="flex justify-between">
-						{group.map((eventIndex) => (
-							<div
-								key={eventIndex}
-								className={`flex items-center justify-center ${itemsToShow === 1 ? 'w-full' : 'w-1/3'
-									} px-2`}
-								onClick={() =>
-									openModal(events[eventIndex], getGradient(eventIndex))
-								}
-							>
-								<div
-									className="text-dark-blue-400 rounded-lg shadow-md max-w-md space-y-6 py-8 px-10 relative"
-									style={{
-										height: '200px',
-										width: '400px',
-										background: getGradient(eventIndex),
-									}}
-								>
-									<div className="flex justify-between items-center px-1 md:px-6">
-										<h3 className="text-4xl font-semibold mb-2 overflow-y-auto">
-											{events[eventIndex].title}
-										</h3>
-										<div className="flex space-x-2">
-											<button
-												className="hover:bg-purple-200 dark:hover:bg-purple-200 p-1.5 rounded"
-												onClick={(e) => {
-													e.stopPropagation();
-													openEditForm(eventIndex); // Open edit form
-												}}
-											>
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													width="20"
-													height="20"
-													viewBox="0 0 24 24"
-													fill="none"
-													stroke="#0F4C75"
-													strokeWidth="2"
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													className="lucide lucide-pencil"
-												>
-													<path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
-													<path d="m15 5 4 4" />
-												</svg>
-											</button>
-											<button
-												className="hover:bg-red-100 dark:hover:bg-red-100 p-1.5 rounded"
-												onClick={(e) => {
-													e.stopPropagation();
-													deletePinnedEvent(eventIndex);
-												}}
-											>
-												<svg
-													xmlns="http://www.w3.org/2000/svg"
-													width="20"
-													height="20"
-													viewBox="0 0 24 24"
-													fill="none"
-													stroke="#ff4d4d"
-													strokeWidth="2"
-													strokeLinecap="round"
-													strokeLinejoin="round"
-													className="lucide lucide-trash-2"
-												>
-													<path d="M3 6h18" />
-													<path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-													<path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-													<line x1="10" x2="10" y1="11" y2="17" />
-													<line x1="14" x2="14" y1="11" y2="17" />
-												</svg>
-											</button>
-										</div>
-									</div>
-									<p
-										className="text-lg font-mono mb-2 overflow-y-auto"
-										style={{ maxHeight: '48px' }}
-									>
-										{events[eventIndex].description}
-									</p>
-									<p className="text-sm font-mono">
-										{formatDate(events[eventIndex].date)}
-									</p>
-								</div>
-							</div>
-						))}
-						{/* Empty divs to fill the space if there are fewer than 3 items */}
-						{itemsToShow === 3 && group.length !== 3 && (
-							<div className="flex items-center justify-center w-1/3 px-2"></div>
-						)}
-						{itemsToShow === 2 && group.length !== 2 && (
-							<div className="flex items-center justify-center w-1/3 px-2"></div>
-						)}
-					</div>
-				))}
-			/>
-			{modalOpen && (
-				<EventModal
-					event={selectedEvent}
-					gradient={selectedGradient}
-					onClose={closeModal}
-				/>
-			)}
-			{formOpen && (
-				<EventForm
-					onClose={closeForm}
-					onSave={handleSave}
-					eventData={selectedEvent}
-				/>
-			)}
-		</>
-	);
+  return (
+    <>
+      <AliceCarousel
+        autoPlay
+        autoPlayInterval={3000}
+        infinite
+        items={groups.map((group, index) => (
+          <div key={index} className="flex justify-between">
+            {group.map((eventIndex) => (
+              <div
+                key={eventIndex}
+                className={`flex items-center justify-center ${
+                  itemsToShow === 1 ? "w-full" : "w-1/3"
+                } px-2`}
+                onClick={() =>
+                  openModal(events[eventIndex], getGradient(eventIndex))
+                }
+              >
+                <div
+                  className="text-dark-blue-400 rounded-lg shadow-md max-w-md space-y-6 py-8 px-10 relative"
+                  style={{
+                    height: "200px",
+                    width: "400px",
+                    background: getGradient(eventIndex),
+                  }}
+                >
+                  <div className="flex justify-between items-center px-1 md:px-6">
+                    <h3 className="text-4xl font-semibold mb-2 overflow-y-auto">
+                      {events[eventIndex].title}
+                    </h3>
+                    <div className="flex space-x-2">
+                      <button
+                        className="hover:bg-purple-200 dark:hover:bg-purple-200 p-1.5 rounded"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          openEditForm(eventIndex); // Open edit form
+                        }}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="#0F4C75"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="lucide lucide-pencil"
+                        >
+                          <path d="M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z" />
+                          <path d="m15 5 4 4" />
+                        </svg>
+                      </button>
+                      <button
+                        className="hover:bg-red-100 dark:hover:bg-red-100 p-1.5 rounded"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          deletePinnedEvent(eventIndex);
+                        }}
+                      >
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="20"
+                          height="20"
+                          viewBox="0 0 24 24"
+                          fill="none"
+                          stroke="#ff4d4d"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          className="lucide lucide-trash-2"
+                        >
+                          <path d="M3 6h18" />
+                          <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
+                          <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
+                          <line x1="10" x2="10" y1="11" y2="17" />
+                          <line x1="14" x2="14" y1="11" y2="17" />
+                        </svg>
+                      </button>
+                    </div>
+                  </div>
+                  <p
+                    className="text-lg font-mono mb-2 overflow-y-auto"
+                    style={{ maxHeight: "48px" }}
+                  >
+                    {events[eventIndex].description}
+                  </p>
+                  <p className="text-sm font-mono">
+                    {formatDate(events[eventIndex].date)}
+                  </p>
+                </div>
+              </div>
+            ))}
+            {/* Empty divs to fill the space if there are fewer than 3 items */}
+            {itemsToShow === 3 && group.length !== 3 && (
+              <div className="flex items-center justify-center w-1/3 px-2"></div>
+            )}
+            {itemsToShow === 2 && group.length !== 2 && (
+              <div className="flex items-center justify-center w-1/3 px-2"></div>
+            )}
+          </div>
+        ))}
+      />
+      {modalOpen && (
+        <EventModal
+          event={selectedEvent}
+          gradient={selectedGradient}
+          onClose={closeModal}
+        />
+      )}
+      {formOpen && (
+        <EventForm
+          onClose={closeForm}
+          onSave={handleSave}
+          eventData={selectedEvent}
+        />
+      )}
+    </>
+  );
 }
 
 export default PinnedEventsCarousel;
